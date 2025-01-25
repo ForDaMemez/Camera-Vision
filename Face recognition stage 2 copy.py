e8bdaa3637e022ea2bff0a98eb622826a1138eda
ForDaMemez <coolchinnu09@gmail.com>
2025-01-25 10:46:13 +0000

    Add files via upload

--- Face recognition stage 2 copy.py
+++ Face recognition stage 2 copy.py
@@ -13,17 +13,17 @@ for subdirs,dirs,files in os.walk(datasets):
       path = subjectpath + "/" + files
       label = id
       labels.append(label)
-      images.append(cv2.imread(path))
+      images.append(cv2.imread(path,0))
     id += 1
 print(names)
-print(images)
+#print(images)
 print(labels)
-images,labels = [np.array(lis) for lis in [images,labels]]
+images,labels = [np.array(lis,dtype = object) for lis in [images,labels]]     
+labels = np.array(labels,dtype= np.int32)
 model = cv2.face.LBPHFaceRecognizer_create()
 model.train(images,labels)
-"""face_detection = cv2.CascadeClassifier(Facexml)
+face_detection = cv2.CascadeClassifier(Facexml)
 facevid = cv2.VideoCapture(0)
-count = 1
 while facevid.isOpened():
   ret,img = facevid.read()
   img1 = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -32,13 +32,13 @@ while facevid.isOpened():
   for x,y,w,h in face:
     rectangle = cv2.rectangle(img,(x,y), (x+w,y+h),(255,0,0), 1)
     person = img1[y:y+h, x:x+w]
-    #personresize = cv2.resize(person, (150,150))
+    personresize = cv2.resize(person, (150,150))
+    prediction = model.predict(personresize)
+    print(prediction)
+    if prediction[1] > 40:
+      cv2.putText(img, names[prediction[0]], (x,y-10), cv2.FONT_HERSHEY_COMPLEX,2,(255,0,0))
     #os.chdir(path)
-    cv2.imwrite(f"{path}/{count}.png",person)
-    count = count + 1
   cv2.imshow("1", img)
   key = cv2.waitKey(5)
   if key == 27:
     break
-  if count >= 31:
-    break"""
