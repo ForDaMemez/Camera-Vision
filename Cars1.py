b9cb2c8e0ac6d62b7539bcbcc997b5977712b74e
ForDaMemez <coolchinnu09@gmail.com>
2025-02-01 10:59:33 +0000

    Add files via upload

---
+++ Cars1.py
@@ -0,0 +1,20 @@
+import cv2
+import os
+import numpy as np
+import cv2
+import os
+import numpy as np
+Carxml = "Assets/cars.xml"
+Vid = "Assets/cars.mp4"
+vid1 = cv2.VideoCapture("Assets/cars.mp4")
+car_detection = cv2.CascadeClassifier(Carxml)
+while vid1.isOpened():
+  ret,img = vid1.read()
+  vid2 = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+  car = car_detection.detectMultiScale(vid2, 1.1,1)
+  for x,y,h,w in car:
+    rectangle = cv2.rectangle(img,(x,y), (x+w,y+h),(255,0,0), 1)
+  cv2.imshow("window1",img)
+  key = cv2.waitKey(30)
+  if key == 27:
+    break
